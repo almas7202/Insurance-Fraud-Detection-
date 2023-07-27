1005ac9b16fa2e786fcac543c24f237dcbc1983a
almas7202 <almasshaikh07202@gmail.com>
2023-07-28 00:20:26 +0530

    Some setup of register

--- InsuranceApp/urls.py
+++ InsuranceApp/urls.py
@@ -4,6 +4,9 @@ from . import views
 from InsuranceApp.views import *
 
 urlpatterns = [
-    path('base/',views.baseview,name='baseview')
+    path('base/',views.baseview,name='baseview'),
+    path('index/',views.indexview,name='indexview'),
+    path('auth/',views.authview,name='authview')
+
     
 ]
--- InsuranceApp/views.py
+++ InsuranceApp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def baseview(request):
     return render(request,'base.html')
+
+
+def indexview(request):
+    return render(request,'index.html')
+
+def authview(request):
+    return render(request,'authentication.html')
