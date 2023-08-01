4547c039dc892cd76278e4860e0a625951cf71d5
almas7202 <almasshaikh07202@gmail.com>
2023-08-01 23:27:27 +0530

    succesfully completed login & Register

--- InsuranceApp/views.py
+++ InsuranceApp/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .form import *
 
 # Create your views here.
 def baseview(request):
@@ -9,8 +16,23 @@ def indexview(request):
     return render(request,'index.html')
 
 def registerview(request):
-    return render(request,'register.html')
-
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 def loginview(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # login(request, user)
+            return redirect('/index/')  # Redirect to the home page after successful login
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+    return render(request, 'login.html')
--- InsuranceProject/settings.py
+++ InsuranceProject/settings.py
@@ -99,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 
 # Internationalization
